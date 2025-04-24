5d80c00500f17a0dbe063701c409e1a75e04edc0
IgrisAbhisheksingh <ABHISHEK8801108@GMAIL.COM>
2025-04-24 17:46:18 +0000

    container config done

---
+++ start/01_variable.js
@@ -0,0 +1,7 @@
+const accountId = "12345";
+let accountEmail = "barakigris@123";
+var accountPass = "45678";
+let accountAddress = "jaipur";
+let rollNo;
+
+console.table([accountId, accountPass, accountEmail, accountAddress,rollNo]);
