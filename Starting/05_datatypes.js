61585bade725c98b9c0854b705867a3110912a32
IgrisAbhisheksingh <ABHISHEK8801108@GMAIL.COM>
2025-04-26 11:07:07 +0000

    datatypes

---
+++ Starting/05_datatypes.js
@@ -0,0 +1,8 @@
+const heros=("indiam airforce","indian army","indian navvy");
+let myObj= {
+    name: "abhishek ",
+    age: 22,
+}
+const myFunction=function(){
+    console.log("hello abhishek");
+}
